1ab74709c8be8be6108e36fc70bae647d3307d9b
Samuel Sjoberg <samuel.sjoberg@extendaretail.com>
2019-11-20 15:12:04 +0100

    fix: Fix Maven to work without POM (#4)

--- maven/src/index.js
+++ maven/src/index.js
@@ -1,4 +1,5 @@
 const core = require('@actions/core');
+const fs = require('fs');
 const mvn = require('./mvn');
 const { checkEnv, versions } = require('../../utils');
 
@@ -6,21 +7,26 @@ const setVersion = async (version) => core.group(
   `Set version ${version}`,
   async () => mvn.setVersion(version));
 
+// Determine if a POM exists. If file argument is given, assume it is valid. If none set, look for pom in pwd.
+const pomExists = (args) =>  args.includes('-f ') || args.includes('--file=') || fs.existsSync('pom.xml');
+
 const run = async() => {
   const args = core.getInput('args', { required: true });
   const version = core.getInput('version');
   try {
     checkEnv(['NEXUS_USERNAME', 'NEXUS_PASSWORD']);
 
+    const hasPom = pomExists(args);
+
     if (!process.env.MAVEN_INIT) {
       await mvn.copySettings();
       core.exportVariable('MAVEN_INIT', 'true');
-      if (!version) {
+      if (!version && hasPom) {
         await versions.getBuildVersion('-SNAPSHOT').then(setVersion);
       }
     }
 
-    if (version) {
+    if (hasPom && version && version !== 'pom.xml') {
       await setVersion(version);
     }
 
