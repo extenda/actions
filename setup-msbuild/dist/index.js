16a89afa98f3b70ec14220d17af21bfec3f18938
Samuel Sjoberg <samuel.sjoberg@extendaretail.com>
2019-11-20 13:17:15 +0100

    Add logging in setup-msbuild

--- setup-msbuild/dist/index.js
+++ setup-msbuild/dist/index.js
@@ -3818,7 +3818,9 @@ const downloadVSWhereIfMissing = async (vswhere) => {
   if (!vswhere) {
     core.info(`Downloading VSWhere ${VSWHERE_VERSION}`);
     const download = await tc.downloadTool(`https://github.com/microsoft/vswhere/releases/download/${VSWHERE_VERSION}/vswhere.exe`);
-    await tc.cacheFile(download, 'vswhere.exe', 'vswhere', VSWHERE_VERSION);
+    core.info(`Downloaded ${download}`);
+    const cachedDir = await tc.cacheFile(download, 'vswhere.exe', 'vswhere', VSWHERE_VERSION);
+    core.info(`VSWhere stored in ${cachedDir}`);
     return findVSWhere();
   }
   return vswhere;
