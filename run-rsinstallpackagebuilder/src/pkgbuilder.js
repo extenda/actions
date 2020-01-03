dd47d5d6cb6f8c2635b291ea76d50f4ca6f5bb94
Thomas Hedemann Hansen <thomas.h.hansen@extendaretail.com>
2020-01-03 09:38:13 +0100

    Refactor and tests for getbinaryName added. Issues with testing of getbuilder()

--- run-rsinstallpackagebuilder/src/pkgbuilder.js
+++ run-rsinstallpackagebuilder/src/pkgbuilder.js
@@ -1,9 +1,9 @@
-// const core = require('@actions/core');
-// const exec = require('@actions/exec');
 const os = require('os');
+const { loadTool } = require('../../utils');
 
 const getBinaryName = async () => {
   let binaryName = '';
+
   if (os.platform() !== 'win32') {
     binaryName = 'InstallerPackageBuilder.Core.Console';
   } else {
@@ -13,4 +13,21 @@ const getBinaryName = async () => {
   return binaryName;
 };
 
-module.exports = getBinaryName;
+const getBuilder = async (appName, binaryVersion) => {
+  const binaryName = getBinaryName();
+  const downloadUrlNexus = `https://repo.extendaretail.com/repository/raw-hosted/${appName}/${binaryVersion}/${binaryName}`;
+
+  const builder = await loadTool({
+    tool: 'InstallerPackageBuilder.Core.Console',
+    binary: binaryName,
+    version: binaryVersion, // Version must be semver and 'latest' is not.
+    downloadUrl: downloadUrlNexus,
+  });
+
+  return builder;
+};
+
+module.exports = {
+  getBinaryName,
+  getBuilder,
+};
--- run-rsinstallpackagebuilder/test/pkgbuilder.test.js
+++ run-rsinstallpackagebuilder/test/pkgbuilder.test.js
@@ -1,17 +1,37 @@
-// const path = require('path');
-// const axios = require('axios');
+const os = require('os');
 
-// jest.mock('os');
+jest.mock('os');
 
-// const { pkgbuilder } = require('../src/pkgbuilder');
+const { getBinaryName /* , getBuilder */ } = require('../src/pkgbuilder');
 
-// describe('RS installer package tests', () => {
-//   test('Get binary name for OS', async () => {
-//     const binaryName = await pkgbuilder.getBinaryName();
-//     // const status = await pkgbuilder(path.join(__dirname, 'report-task.txt'), 0);
-//     expect(binaryName).toEqual('InstallerPackageBuilder.Core.Console.exe');
-//   });
+// const binaryVersion = 'v1.0.0';
+// const appName = 'RS.InstallerPackageBuilder.Core.Console';
 
-// //   test('SonarQube', async () => {
-// //   });
-// });
+describe('RS installer package tests', () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
+  describe('Windows platform', () => {
+    test('Get binary name for win32', async () => {
+      os.platform.mockImplementation(() => 'win32');
+      const binaryName = await getBinaryName();
+      expect(binaryName).toEqual('InstallerPackageBuilder.Core.Console.exe');
+    });
+
+    // TODO: Cannot get this test to validate the response from getbuilder.
+    // test('Get installer package builder for win32', async () => {
+    //   os.platform.mockImplementation(() => 'win32');
+    //   const builder = await getBuilder(appName, binaryVersion);
+    //   expect(builder).toEqual(null);
+    // });
+  });
+
+  describe('Linux platform', () => {
+    test('Get binary name for linux', async () => {
+      os.platform.mockImplementation(() => 'linux');
+      const binaryName = await getBinaryName();
+      expect(binaryName).toEqual('InstallerPackageBuilder.Core.Console');
+    });
+  });
+});
