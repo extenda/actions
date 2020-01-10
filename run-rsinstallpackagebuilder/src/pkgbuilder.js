23b0ba16feca507a871ce8c8a7fbbd4fc412cbcd
Samuel Sjoberg <samuel.sjoberg@extendaretail.com>
2020-01-10 15:59:41 +0100

    Fix rs package builder (WIP)

--- run-rsinstallpackagebuilder/src/pkgbuilder.js
+++ run-rsinstallpackagebuilder/src/pkgbuilder.js
@@ -1,17 +1,118 @@
 const os = require('os');
+const fs = require('fs');
+const path = require('path');
+const exec = require('@actions/exec');
+// const { loadTool } = require('../../utils');
+const axios = require('axios');
+const { promisify } = require('util');
 
-const getBinaryName = async () => {
-  let binaryName = '';
+const BINARY_NAME = os.platform() !== 'win32'
+  ? 'InstallerPackageBuilder.Core.Console'
+  : 'InstallerPackageBuilder.Core.Console.exe';
 
-  if (os.platform() !== 'win32') {
-    binaryName = 'InstallerPackageBuilder.Core.Console';
-  } else {
-    binaryName = 'InstallerPackageBuilder.Core.Console.exe';
-  }
+const packageBuilderCommand = async (builder, args) => {
+  const {
+    btype,
+    packageName,
+    workingDir,
+    outputDir,
+    sourcePaths,
+    sourceFilePaths,
+  } = args;
 
-  return binaryName;
+  return exec.exec(
+    builder,
+    [
+      btype,
+      '-pn', packageName,
+      '-wd', workingDir,
+      '-od', outputDir,
+      '-sp', sourcePaths,
+      '-sfp', sourceFilePaths,
+    ],
+  );
 };
 
-module.exports = {
-  getBinaryName,
+// const http = require('http');
+
+// const download = (url, dest, cb) => {
+//   const file = fs.createWriteStream(dest);
+//   const { NEXUS_USERNAME, NEXUS_PASSWORD } = process.env;
+//   //const auth = Buffer.from(`${NEXUS_USERNAME}:${NEXUS_PASSWORD}`).toString('base64');
+//   const auth = `${NEXUS_USERNAME}:${NEXUS_PASSWORD}`;
+//
+//   http.get(url, { auth }, (response) => {
+//     response.pipe(file);
+//     file.on('finish', () => {
+//       file.close(cb); // close() is async, call cb after close completes.
+//     });
+//   }).on('error', (err) => {
+//     fs.unlink(dest); // Delete the file async. (But we don't check the result)
+//     cb(err.message);
+//   });
+// };
+
+const loadTool = async ({ binaryVersion, outputDir }) => {
+  // FIXME: I haven't got this working yet. The stream never fires any completion events. I'm just too bad at async JS :(
+
+  const appName = 'RS.InstallerPackageBuilder.Core.Console';
+
+  const buildTool = path.join(outputDir, BINARY_NAME);
+  const stream = fs.createWriteStream(buildTool);
+  // TODO Check if file exists and reuse in that case.
+
+  console.log(`https://repo.extendaretail.com/repository/raw-hosted/${appName}/${binaryVersion}/${BINARY_NAME}`)
+
+  const url = `https://repo.extendaretail.com/repository/raw-hosted/${appName}/${binaryVersion}/${BINARY_NAME}`;
+
+  // await new Promise((resolve, reject) => {
+  //   download(url, buildTool, resolve, reject);
+  // });
+
+
+  const response = await axios.get(
+    `https://repo.extendaretail.com/repository/raw-hosted/${appName}/${binaryVersion}/${BINARY_NAME}`,
+    {
+      auth: {
+        username: process.env.NEXUS_USERNAME,
+        password: process.env.NEXUS_PASSWORD,
+      },
+      responseType: 'stream',
+    },
+  );
+
+  console.log('Got response');
+
+  //fs.writeFileSync(buildTool, response.data);
+
+  console.log('Wait for pipe');
+  //
+  // stream.on('open', () => {
+  //   console.log('Start streaming!');
+  //   response.data.pipe(stream);
+  // });
+
+  const p = new Promise((resolve, reject) => {
+    stream.on('close', resolve);
+    stream.on('error', reject);
+    stream.on('data', () => { console.log('Data'); });
+  });
+
+  response.data.pipe(stream);
+
+  await p;
+
+  console.log('CHMOD');
+  fs.accessSync(buildTool, 777);
+
+  console.log('Downloaded', buildTool);
+
+  return buildTool;
 };
+
+const buildPackage = async (args) => {
+  const buildTool = await loadTool(args);
+  return packageBuilderCommand(buildTool, args);
+};
+
+module.exports = buildPackage;
--- run-rsinstallpackagebuilder/test/pkgbuilder.test.js
+++ run-rsinstallpackagebuilder/test/pkgbuilder.test.js
@@ -1,27 +1,43 @@
 const os = require('os');
 const path = require('path');
 const fs = require('fs');
-const { run } = require('../src/index');
+const fsExtra = require('fs-extra');
+
+// Used by @actions/tool-cache. Directory must exist before we load module.
+const outputDir = path.join(__dirname, '..', 'test_output_dir');
+process.env.RUNNER_TEMP = outputDir;
+process.env.RUNNER_TOOL_CACHE = outputDir;
+
+if (!fs.existsSync(outputDir)) {
+  fs.mkdirSync(outputDir);
+}
+
+const buildPackage = require('../src/pkgbuilder');
+
+jest.setTimeout(30000);
 
 describe('RS installer package tests', () => {
-  beforeEach(() => {
-    jest.resetModules();
-  });
   test('Run builder on test files', async () => {
-    jest.setTimeout(100000);
-    const outputDir = path.join(__dirname, '..', 'test_output_dir');
-    process.env['INPUT_PACKAGE-NAME'] = 'Test_PkgName';
-    process.env['INPUT_BUILDER-TYPE'] = 'single';
-    process.env['INPUT_WORKING-DIR'] = os.tmpdir();
-    process.env['INPUT_OUTPUT-DIR'] = outputDir;
-    process.env['INPUT_SOURCE-PATHS'] = __dirname;
-    process.env['INPUT_SOURCE-FILEPATHS'] = '';
-    process.env['INPUT_TOOL-VERSION'] = '1.0.0';
+    fsExtra.removeSync(outputDir);
+    fs.mkdirSync(outputDir);
 
-    // process.env['NEXUS_USERNAME'] = '';
-    // process.env['NEXUS_PASSWORD'] = '';
+    if (!process.env.NEXUS_USERNAME) {
+      console.log('Using local user credentials in test');
+      process.env.NEXUS_USERNAME = process.env.GITHUB_USER;
+      process.env.NEXUS_PASSWORD = process.env.GITHUB_TOKEN;
+    }
 
-    await run(); // Should create a package of this file and place it under ../test_output_dir.
+    // Should create a package of this file and place it under ../test_output_dir.
+    await buildPackage({
+      builderType: 'single',
+      binaryVersion: '1.0.0',
+      packageName: 'Test_PkgName',
+      workingDir: os.tmpdir(),
+      outputDir,
+      sourcePaths: __dirname,
+      sourceFilePaths: '',
+    });
+
     expect(fs.existsSync(path.join(outputDir, 'Test_PkgName.pkg.xml'))).toBe(true);
   });
 });
