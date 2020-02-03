043d8bd2c74bb6fdfb7812834d8e508f555a22e7
Samuel Sjoberg <samuel.sjoberg@extendaretail.com>
2020-02-03 22:23:15 +0100

    feat: Add gcp-secret-manager action

---
+++ gcp-secret-manager/README.md
@@ -0,0 +1,41 @@
+# gcp-secret-manager
+
+This GitHub Action can be used to access secrets in [Google Cloud Secret Manager](https://cloud.google.com/secret-manager/docs) 
+in your GitHub Actions Workflow.
+
+The action takes a key-value pair of environment variables mapped to secret names. It will populate all the 
+environment variables with the secret value and make them available in subsequent steps.
+
+## Usage
+
+See [action.yml](action.yml).
+
+### Secrets
+
+This action requires a GCP service account key with permission to access secret payloads.
+Once created, the JSON key should be `base64` encoded and added as secret in the GitHub repository.
+
+It is recommended that the service account _only_ has permissions to access secrets. Do not allow modifications or 
+access to any other resources in your project.
+  
+### Examples
+
+#### Basic Usage
+
+```yaml
+on: push
+
+jobs:
+  test:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v1
+
+    - uses: extenda/actions/gcp-secret-manager@v0
+      with:
+        service-account-key: ${{ secrets.SECRET_AUTH }}
+        secrets: |
+          NEXUS_PASSWORD: nexus-password
+          NEXUS_USERNAME: nexus-username
+          SONAR_TOKEN: sonarsource-token
+```
---
+++ gcp-secret-manager/src/secrets.js
@@ -0,0 +1,52 @@
+const core = require('@actions/core');
+const YAML = require('yaml');
+const fs = require('fs');
+const tmp = require('tmp');
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+
+tmp.setGracefulCleanup();
+
+let client;
+
+const createKeyFile = (serviceAccountKey) => {
+  const tmpFile = tmp.fileSync({ postfix: '.json' });
+  const jsonKey = Buffer.from(serviceAccountKey, 'base64').toString('utf8');
+  fs.writeFileSync(tmpFile.name, jsonKey);
+  return tmpFile.name;
+};
+
+const createClient = (serviceAccountKey) => {
+  const keyFilename = createKeyFile(serviceAccountKey);
+  client = new SecretManagerServiceClient({
+    keyFilename,
+  });
+};
+
+const accessSecretValue = async (projectId, name) => {
+  const [version] = await client.accessSecretVersion({
+    name: client.secretVersionPath(projectId, name, 'latest'),
+  });
+  return version.payload.data.toString('utf8');
+};
+
+const parseInputYaml = (secretsYaml) => YAML.parse(secretsYaml);
+
+const loadSecrets = async (serviceAccountKey, secrets = {}) => {
+  createClient(serviceAccountKey);
+  const projectId = await client.auth.getProjectId();
+  const results = [];
+  Object.keys(secrets).forEach((env) => {
+    const name = secrets[env];
+    results.push(accessSecretValue(projectId, name)
+      .then((secret) => {
+        core.setSecret(secret);
+        core.exportVariable(env, secret);
+      }));
+  });
+  return Promise.all(results);
+};
+
+module.exports = {
+  loadSecrets,
+  parseInputYaml,
+};
