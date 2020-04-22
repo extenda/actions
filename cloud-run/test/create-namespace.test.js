a24318db38705be43889c707f4ca63aab5d0f9ff
Santos92 <AlexanderLundbergSantos@live.se>
2020-04-22 14:03:18 +0200

    fix: Linting

--- cloud-run/test/create-namespace.test.js
+++ cloud-run/test/create-namespace.test.js
@@ -11,7 +11,7 @@ const clusterInfo = {
 
 const mockOutput = (data, opts) => {
   opts.listeners.stderr(Buffer.from(`${data}\n`, 'utf8'));
-  return Promise.reject(new Error("exit code 1"));
+  return Promise.reject(new Error('exit code 1'));
 };
 
 describe('Create namespace', () => {
