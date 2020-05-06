1b911a47e603b4f7f9f29ea354504ee5582a8a52
Samuel Sjoberg <samuel.sjoberg@extendaretail.com>
2020-05-06 12:19:35 +0200

    feat: Add export-default-credentials input to setup-gcloud (#99)

--- setup-gcloud/src/index.js
+++ setup-gcloud/src/index.js
@@ -5,5 +5,6 @@ const setupGcloud = require('./setup-gcloud');
 run(async () => {
   const serviceAccountKey = core.getInput('service-account-key', { required: true });
   const version = core.getInput('version') || 'latest';
-  await setupGcloud(serviceAccountKey, version);
+  const exportCredentials = core.getInput('export-default-credentials') || 'false';
+  await setupGcloud(serviceAccountKey, version, exportCredentials === 'true');
 });
