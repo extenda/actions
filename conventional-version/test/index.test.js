80e9159e89a324cc15af9974f80c3cb1a43fe580
espenpedersenER <57531533+espenpedersenER@users.noreply.github.com>
2020-06-09 23:31:33 +0200

    fix: Code cleanup

--- conventional-version/test/index.test.js
+++ conventional-version/test/index.test.js
@@ -1,19 +1,5 @@
 jest.mock('@actions/core');
-//jest.mock('../../utils/src/branch-info');
-
-const core = require('@actions/core');
 const action = require('../src/index');
-const branchinfo = require('../../utils/src/branch-info');
-// const {
-//   getBranchName,
-//   getBranchNameFriendly,
-//   getBranchNameSemver,
-//   getBranchNameShort,
-//   getBranchType,
-//   getComposedVersionString,
-//   getShortSha,
-//   getTagAtCommit,
-// } = require('../../utils/src/branch-info');
 
 const orgEnv = process.env;
 
@@ -26,29 +12,10 @@ describe('create packaage Action', () => {
       GITHUB_SHA: '300ef1336f23588c9f4dc347989006033cea780d',
     };
   });
-  //     getBranchName.mockResolvedValueOnce('feature-branch-1');
-  //     getBranchNameFriendly.mockResolvedValueOnce('refs/heads');
-  //     getBranchNameSemver.mockResolvedValueOnce('featurebranch1');
-  //     getBranchNameShort.mockResolvedValueOnce('feature-branch-1');
-  //     getBranchType.mockResolvedValueOnce('develop');
-  //     getComposedVersionString.mockResolvedValueOnce('1.0.0-feature-branch-1-shalala');
-  //     getShortSha.mockResolvedValueOnce('shalala');
-  //     getTagAtCommit.mockResolvedValueOnce('tag1');
-
   afterEach(() => {
     jest.resetAllMocks();
     process.env = orgEnv;
   });
-
-  //   test('It fails for missing GITHUB_REF', async () => {
-  //     delete process.env.GITHUB_REF;
-  // await expect(action()).rejects.toEqual(new Error('Missing env var: GITHUB_REF'));
-  //   });
-
-  //   test('It fails for missing GITHUB_HEAD_REF', async () => {
-  //     delete process.env.GITHUB_HEAD_REF;
-  //     await expect(action()).rejects.toEqual(new Error('Missing env var: GITHUB_HEAD_REF'));
-  //   });
 
   test('It will use passed inputs', async () => {
     await action();
