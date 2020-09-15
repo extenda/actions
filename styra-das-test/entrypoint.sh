c2be7aa19b96fcc4520f8e000eb57b9083224291
Samuel Sjoberg <samuel.sjoberg@extendaretail.com>
2020-09-16 00:54:32 +0200

    fix: Switch search to use styra CLI

--- styra-das-test/entrypoint.sh
+++ styra-das-test/entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 set -e
 
-# Lookup the System ID from styra APIs.
-SYSTEM_ID=$(curl -sS -H "Authorization: Bearer ${STYRA_TOKEN}" \
-    "${STYRA_ORGANIZATION_ID}/v1/systems?compact=true&name=${STYRA_SYSTEM_NAME}" \
-  | jq -r '.result[0].id // "NotFound"')
+# Lookup the System ID from system name.
+SYSTEM_ID=$(/usr/local/bin/styra-cli get systems | grep "$STYRA_SYSTEM_NAME" | awk '{print $1}')
 
-if [ "$SYSTEM_ID" = "NotFound" ]; then
+if [ -z "$SYSTEM_ID" ]; then
   echo "Not found. No system matching name '${STYRA_SYSTEM_NAME}'"
   exit 1
 fi
