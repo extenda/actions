2b2e1050fbd407acbd5908fb8f92f8460bdc5b5a
Samuel Sjoberg <samuel.sjoberg@extendaretail.com>
2020-09-23 09:42:09 +0200

    feat(ELY-497): Validate alphabetic ordering in IAM definitions (#226)

--- iam/src/iam-definition.js
+++ iam/src/iam-definition.js
@@ -11,9 +11,40 @@ const loadFile = (iamFile) => {
   return yaml.parse(fs.readFileSync(iamFile, 'utf8'));
 };
 
+const sortAndCompare = (array, docPath, result) => {
+  const sorted = array.slice(0).sort((a, b) => a.localeCompare(b, 'en-US'));
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] !== sorted[i]) {
+      const err = result.addError('is not sorted alphabetically');
+      err.property = `instance.${docPath}[${i}]`;
+    }
+  }
+};
+
+const validateAlphaSort = (spec, result) => {
+  const {
+    services = [],
+    permissions = {},
+    roles = [],
+  } = spec;
+
+  sortAndCompare(services.map((service) => service.name), 'services', result);
+
+  sortAndCompare(Object.keys(permissions), 'permissions', result);
+  Object.keys(permissions).forEach((noun) => {
+    sortAndCompare(permissions[noun], `permissions.${noun}`, result);
+  });
+
+  sortAndCompare(roles.map((role) => role.id), 'roles', result);
+  roles.forEach((role, index) => {
+    sortAndCompare(role.permissions, `roles[${index}].permissions`, result);
+  });
+};
+
 const validateSchema = (iamFile, spec) => {
   core.info(`Validate ${iamFile}`);
   const result = validate(spec, jsonSchema);
+  validateAlphaSort(spec, result);
   if (!result.valid) {
     const message = `${iamFile} is not valid.\n${result.toString()}`;
     core.error(message);
