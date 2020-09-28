f91cb2b30305ade9984d7eecf032b3e80500452a
Aleksandr Skoriy <askoriy@users.noreply.github.com>
2020-09-28 08:26:05 +0300

    feat(ELY-516): Make a copy of plan file with changes (#233)

--- terraform-plan-comment/test/generate-outputs.test.js
+++ terraform-plan-comment/test/generate-outputs.test.js
@@ -1,4 +1,5 @@
 const mockFs = require('mock-fs');
+const fs = require('fs');
 const exec = require('@actions/exec');
 const generateOutputs = require('../src/generate-outputs');
 
@@ -54,8 +55,12 @@ describe('Generate Terraform plan output', () => {
 
     const outputs = await generateOutputs('/work', 'plan.out');
     expect(outputs).toEqual([
-      { module: 'moduleA', output: 'Module A changes', status: 0 },
-      { module: 'moduleB', output: 'Module B changes', status: 0 },
+      {
+        module: 'moduleA', output: 'Module A changes', status: 0, plan: '/work/moduleA/plan.out',
+      },
+      {
+        module: 'moduleB', output: 'Module B changes', status: 0, plan: '/work/moduleB/plan.out',
+      },
     ]);
     expect(exec.exec.mock.calls[0][1]).toEqual(['show', '-no-color', '../../../plan.out']);
     expect(exec.exec.mock.calls[0][2]).toMatchObject({
@@ -65,6 +70,9 @@ describe('Generate Terraform plan output', () => {
     expect(exec.exec.mock.calls[1][2]).toMatchObject({
       cwd: '/work/moduleB/.terragrunt-cache/UUID1/UUID2',
     });
+    // expect(fs.exists('/work/moduleA/plan.out.changes'));
+    expect(fs.existsSync('/work/moduleA/plan.out.changes')).toEqual(true);
+    expect(fs.existsSync('/work/moduleB/plan.out.changes')).toEqual(true);
   });
 
   test('It can process a single plan file', async () => {
@@ -75,7 +83,9 @@ describe('Generate Terraform plan output', () => {
     const outputs = await generateOutputs('/path/to/work', 'plan.out');
     expect(outputs).toHaveLength(1);
     expect(outputs).toEqual([
-      { module: 'work', output: 'Changed terraform plan.', status: 0 },
+      {
+        module: 'work', output: 'Changed terraform plan.', status: 0, plan: '/path/to/work/plan.out',
+      },
     ]);
     expect(exec.exec).toHaveBeenCalledTimes(1);
   });
@@ -94,8 +104,12 @@ describe('Generate Terraform plan output', () => {
     expect(exec.exec).toHaveBeenCalledTimes(2);
     expect(outputs).toHaveLength(1);
     expect(outputs).toEqual([
-      { module: 'moduleB', output: 'Module B changes', status: 0 },
+      {
+        module: 'moduleB', output: 'Module B changes', status: 0, plan: '/work/moduleB/plan.out',
+      },
     ]);
+    expect(fs.existsSync('/work/moduleA/plan.out.changes')).toEqual(false);
+    expect(fs.existsSync('/work/moduleB/plan.out.changes')).toEqual(true);
   });
 
   test('It will filter changing ignored objects', async () => {
@@ -113,7 +127,12 @@ describe('Generate Terraform plan output', () => {
     expect(exec.exec).toHaveBeenCalledTimes(2);
     expect(outputs).toHaveLength(1);
     expect(outputs).toEqual([
-      { module: 'moduleA', output: '# module.module_a.resource.resource_a must be replaced\nModule A Plan: 1 to add, 0 to change, 1 to destroy.', status: 0 },
+      {
+        module: 'moduleA',
+        output: '# module.module_a.resource.resource_a must be replaced\nModule A Plan: 1 to add, 0 to change, 1 to destroy.',
+        status: 0,
+        plan: '/work/moduleA/plan.out',
+      },
     ]);
   });
 
@@ -125,7 +144,9 @@ describe('Generate Terraform plan output', () => {
     const outputs = await generateOutputs('/work', 'plan.out');
     expect(outputs).toHaveLength(1);
     expect(outputs).toEqual([
-      { module: 'work', output: 'Terraform output\nError', status: 1 },
+      {
+        module: 'work', output: 'Terraform output\nError', status: 1, plan: '/work/plan.out',
+      },
     ]);
     expect(exec.exec).toHaveBeenCalledTimes(1);
   });
