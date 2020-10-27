bb6e5199ea5d935a580033b481a838ba7ba60d40
Anastasiia Kovtun <nastia@Anastasiias-MacBook-Pro-2.local>
2020-10-27 19:06:01 +0200

    chore: Add more tests

--- dataflow-template-build/test/dataflow-bulid.test.js
+++ dataflow-template-build/test/dataflow-bulid.test.js
@@ -9,11 +9,38 @@ describe('Build Dataflow template', () => {
 
   test('Build template', async () => {
     dataflowBuild(
-      'template-name',
-      'gcr.io/test-project/my-service:tag',
-      'metadata.json',
+      'gs://test/dataflow/template.json',
+      'gcr.io/project/image:tag',
       'JAVA',
+      'metadata.json',
     );
     expect(exec.exec).toHaveBeenCalledTimes(1);
+    expect(exec.exec).toHaveBeenCalledWith('gcloud', [
+      'dataflow',
+      'flex-template',
+      'build',
+      'gs://test/dataflow/template.json',
+      '--image=gcr.io/project/image:tag',
+      '--sdk-language=JAVA',
+      '--metadata-file=metadata.json',
+    ]);
+  });
+
+  test('Build template without metadata flag', async () => {
+    dataflowBuild(
+      'gs://test/dataflow/template.json',
+      'gcr.io/project/image:tag',
+      'JAVA',
+      '',
+    );
+    expect(exec.exec).toHaveBeenCalledTimes(1);
+    expect(exec.exec).toHaveBeenCalledWith('gcloud', [
+      'dataflow',
+      'flex-template',
+      'build',
+      'gs://test/dataflow/template.json',
+      '--image=gcr.io/project/image:tag',
+      '--sdk-language=JAVA',
+    ]);
   });
 });
