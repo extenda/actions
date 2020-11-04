6859bfafafa6768ca2f9e633e6d0df2400847ffd
Shayne Clausson <shayne.clausson@extendaretail.com>
2020-11-04 12:08:27 +0100

    fix: Use dotnet-sonarscanner version compatible with internal sonar

--- sonar-scanner/src/scan-msbuild.js
+++ sonar-scanner/src/scan-msbuild.js
@@ -42,7 +42,7 @@ const scanWithJavaHome = async (args) => {
 
 const beginScan = async (hostUrl, mainBranch) => {
   await core.group('Install dotnet-sonarscanner', async () => {
-    await exec.exec('dotnet tool install -g dotnet-sonarscanner');
+    await exec.exec('dotnet tool install -g dotnet-sonarscanner --version 4.10.0');
   });
 
   const version = await getBuildVersion(`-${process.env.GITHUB_SHA}`);
