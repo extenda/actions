33db6b8c7906f9f80808bc8ced10e53d2df9f814
Alexander Lundberg Santos <alexander.l.santos@extendaretail.com>
2021-01-11 12:30:11 +0100

    feat: Deploy on cloudrun will also inject the env variable for the pod

--- cloud-run/dist/index.js
+++ cloud-run/dist/index.js
@@ -11287,6 +11287,7 @@ const createEnvironmentArgs = (environment, projectId) => {
     args.push(`${name}=${value}`);
   });
   args.push(`SERVICE_PROJECT_ID=${projectId}`);
+  args.push(`SERVICE_ENVIRONMENT=${projectId.split('-')[1]}`);
   return args.join(',');
 };
 
