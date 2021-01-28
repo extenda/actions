2bc2cfe5e8c9b3a5e8503dbb7b4c44fe3629843a
espenpedersenER <57531533+espenpedersenER@users.noreply.github.com>
2021-01-28 12:58:55 +0100

    fix: Add missing )

--- docker/src/docker.js
+++ docker/src/docker.js
@@ -47,7 +47,7 @@ const login = (registry) => {
   if (isEcr(registry)) {
     const region = getRegion(registry);
     core.info(`Logging into ECR region ${region}...`);
-    cp.execSync(`$(aws ecr get-login-password --region ${region} --no-include-email`);
+    cp.execSync(`$(aws ecr get-login-password --region ${region} --no-include-email)`);
     return;
   }
 
