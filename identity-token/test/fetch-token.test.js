52b345c0855750105881fa4659f91648a2601978
Anastasiia Kovtun <57349964+akovtun-extenda@users.noreply.github.com>
2021-05-31 10:18:57 +0300

    feat: Add action for fetching identity token (#356)
    * feat: Add action for fetching identity token
    
    * fix: Regenerate dist/
    
    * fix: Change desription for audiences

---
+++ identity-token/test/fetch-token.test.js
@@ -0,0 +1,27 @@
+jest.mock('@actions/exec');
+const exec = require('@actions/exec');
+const fetchToken = require('../src/fetch-token');
+
+
+const mockExecListeners = (output) => (cmd, args, opts) => {
+  opts.listeners.stdout(Buffer.from(output, 'utf8'));
+  return Promise.resolve(0);
+};
+
+describe('Obtain an identity token', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test('Fetch token', async () => {
+    const token = 'yJhbGciOiJSUzI1NiIsImtpZCI6Im';
+    exec.exec
+      .mockImplementationOnce(mockExecListeners(token));
+
+    fetchToken(
+      'my-sa@example.iam.gserviceaccount.com',
+      'bhq-braveheart-quotes',
+    );
+    expect(exec.exec).toHaveBeenCalledTimes(1);
+  });
+});
