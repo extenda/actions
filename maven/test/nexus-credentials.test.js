c3ca2c7df8611e638fe2f3d0838fe75a107b43d6
Samuel Sjoberg <samuel.sjoberg@extendaretail.com>
2021-08-18 21:42:21 +0200

    fix: Fix failing test

--- maven/test/nexus-credentials.test.js
+++ maven/test/nexus-credentials.test.js
@@ -6,6 +6,6 @@ test('It loads credentials for Nexus', async () => {
   loadSecretIntoEnv.mockResolvedValueOnce('secret1').mockResolvedValueOnce('secret2');
   await setupCredentials('service-account-key', 'nexus-username', 'nexus-password');
   expect(loadSecretIntoEnv).toHaveBeenCalledTimes(2);
-  expect(loadSecretIntoEnv).toHaveBeenCalledWith('service-account-key', 'nexus-username', 'NEXUS_USERNAME');
-  expect(loadSecretIntoEnv).toHaveBeenCalledWith('service-account-key', 'nexus-password', 'NEXUS_PASSWORD');
+  expect(loadSecretIntoEnv).toHaveBeenCalledWith('service-account-key', 'nexus-username', 'NEXUS_USERNAME', true);
+  expect(loadSecretIntoEnv).toHaveBeenCalledWith('service-account-key', 'nexus-password', 'NEXUS_PASSWORD', true);
 });
