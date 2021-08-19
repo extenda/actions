88b1cd0edc995bcca5ea27901a66c5461bcca321
Samuel Sjoberg <samuel.sjoberg@extendaretail.com>
2021-08-19 15:51:35 +0200

    feat(maven): Remove unused dependencies from cache

--- maven/src/maven-cache.js
+++ maven/src/maven-cache.js
@@ -2,21 +2,58 @@ const os = require('os');
 const path = require('path');
 const core = require('@actions/core');
 const cache = require('@actions/cache');
+const fg = require('fast-glob');
+const { promisify } = require('util');
+const rmdir = promisify(require('fs').rmdir);
+const unlink = promisify(require('fs').unlink);
 
-const paths = [
-  path.join(os.homedir(), '.m2', 'repository'),
-];
+const REPO = path.join(os.homedir(), '.m2', 'repository');
+const RESTORED_TIME = 'maven.RestoredTime';
+const RESTORED_KEY = 'maven.RestoredKey';
 
 const withCacheKey = async (fn) => {
   const cacheKey = core.getInput('cache-key') || '';
   return cacheKey ? fn(cacheKey) : null;
 };
 
-const saveCache = async () => withCacheKey((key) => cache.saveCache(paths, key));
+const saveCache = async () => withCacheKey((key) => {
+  const restoredKey = core.getState(RESTORED_KEY);
+  const restoredTime = Number(core.getState(RESTORED_TIME));
+
+  // Pattern used to remove all lastUpdated markers to always attempt a new
+  // resolution on a build. This ensures we always pull snapshots.
+  const patterns = ['**/*.lastUpdated'];
+
+  if (restoredKey !== key) {
+    // If this isn't a perfect cache hit, we want to remove
+    // unused dependencies from our cache.
+    patterns.push('**/*.pom');
+  }
+
+  return fg(patterns, { cwd: REPO, objectMode: true, stats: true })
+    .then((entries) => Promise.all(entries.map((e) => {
+      if (e.name.endsWith('.lastUpdated')) {
+        const fqn = path.join(REPO, e.path);
+        core.debug(`Remove: ${fqn}`);
+        return unlink(fqn);
+      }
+      if (e.stats.atimeMs < restoredTime) {
+        const fqn = path.dirname(path.join(REPO, e.path));
+        core.debug(`Remove: ${fqn}`);
+        return rmdir(fqn, { recursive: true });
+      }
+      return Promise.resolve();
+    }))).then(() => cache.saveCache([REPO], key));
+});
 
 const restoreCache = async () => withCacheKey((key) => {
   const restoreKeys = (core.getInput('cache-restore-keys') || key).split(/\s+/);
-  return cache.restoreCache(paths, key, restoreKeys);
+  return cache.restoreCache([REPO], key, restoreKeys)
+    .then((restoreKey) => {
+      core.saveState(RESTORED_KEY, restoreKey);
+      core.saveState(RESTORED_TIME, `${Date.now()}`);
+      return restoreKey;
+    });
 });
 
 module.exports = {
--- maven/test/maven-cache.test.js
+++ maven/test/maven-cache.test.js
@@ -1,19 +1,55 @@
+const mockFs = require('mock-fs');
+
 jest.mock('@actions/core');
 jest.mock('@actions/cache');
 
 const { homedir } = require('os');
 const path = require('path');
+const fs = require('fs');
 const core = require('@actions/core');
 const cache = require('@actions/cache');
 const { saveCache, restoreCache } = require('../src/maven-cache');
 
+const now = Date.now();
+
+const REPO_HOME = path.join(homedir(), '.m2', 'repository');
+
 describe('Maven cache', () => {
   beforeEach(() => {
-    jest.resetAllMocks();
+    const files = {};
+    files[`${REPO_HOME}/junit/junit`] = {
+      4.13: {
+        'junit-4.13.pom': mockFs.file({
+          content: '',
+          ctime: now,
+          mtime: now,
+          atime: now + 1,
+        }),
+        'junit-4.13.jar': '',
+        'junit-4.13.jar.lastUpdated': '',
+      },
+      4.12: {
+        'junit-4.12.pom': mockFs.file({
+          content: '',
+          ctime: new Date(1),
+          mtime: new Date(1),
+          atime: new Date(1),
+        }),
+        'junit-4.12.jar': '',
+        'junit-4.12.jar.lastUpdated': '',
+      },
+    };
+    mockFs(files);
   });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+    mockFs.restore();
+  })
 
   test('It uses cache inputs', async () => {
     core.getInput.mockReturnValue('value');
+    cache.restoreCache.mockResolvedValueOnce('value');
     await restoreCache();
     expect(core.getInput).toHaveBeenCalledWith('cache-key');
     expect(core.getInput).toHaveBeenCalledWith('cache-restore-keys');
@@ -22,6 +58,7 @@ describe('Maven cache', () => {
   test('It will restore a cache', async () => {
     core.getInput.mockReturnValueOnce('test-maven-12345')
       .mockReturnValueOnce('test-maven-');
+    cache.restoreCache.mockResolvedValueOnce('test-maven-');
     await restoreCache();
     expect(cache.restoreCache).toHaveBeenCalledWith(
       [path.join(homedir(), '.m2', 'repository')],
@@ -33,6 +70,7 @@ describe('Maven cache', () => {
   test('It handles multiple restore keys', async () => {
     core.getInput.mockReturnValueOnce('test-maven-12345')
       .mockReturnValueOnce('test-maven-\ntest-  test-maven-123');
+    cache.restoreCache.mockResolvedValueOnce('test-maven-');
     await restoreCache();
     expect(cache.restoreCache.mock.calls[0][2]).toEqual(['test-maven-', 'test-', 'test-maven-123']);
   });
@@ -40,6 +78,7 @@ describe('Maven cache', () => {
   test('It handles empty restore keys', async () => {
     core.getInput.mockReturnValueOnce('test-maven-12345')
       .mockReturnValueOnce('');
+    cache.restoreCache.mockResolvedValueOnce('test-maven-');
     await restoreCache();
     expect(cache.restoreCache.mock.calls[0][2]).toEqual(['test-maven-12345']);
   });
@@ -58,10 +97,62 @@ describe('Maven cache', () => {
 
   test('It saves cache for given cache-key', async () => {
     core.getInput.mockReturnValueOnce('test-maven-12345');
+    cache.saveCache.mockResolvedValueOnce('12345');
     await saveCache();
     expect(cache.saveCache).toHaveBeenCalledWith(
       [path.join(homedir(), '.m2', 'repository')],
       'test-maven-12345',
     );
+  });
+
+  test('It saves state after restore', async () => {
+    core.getInput.mockReturnValueOnce('test-maven-12345')
+      .mockReturnValueOnce('test-maven-');
+    cache.restoreCache.mockResolvedValueOnce('test-maven-');
+    await restoreCache();
+    expect(core.saveState).toHaveBeenCalledWith('maven.RestoredKey', 'test-maven-');
+    expect(core.saveState).toHaveBeenCalledWith('maven.RestoredTime', expect.stringMatching(/\d+/));
+  });
+
+  test('It skips housekeeping for exact cache hits', async () => {
+    // Last updated marker exists before test.
+    expect(fs.existsSync(path.join(REPO_HOME, 'junit', 'junit', '4.12', 'junit-4.12.jar.lastUpdated'))).toEqual(true);
+
+    core.getInput.mockReturnValueOnce('test-maven-12345');
+    core.getState.mockReturnValueOnce('test-maven-12345')
+      .mockReturnValueOnce(`${now}`);
+    cache.saveCache.mockResolvedValueOnce('12345');
+    await saveCache();
+
+    expect(core.getState).toHaveBeenNthCalledWith(1, 'maven.RestoredKey');
+    expect(core.getState).toHaveBeenNthCalledWith(2, 'maven.RestoredTime');
+
+    // No housekeeping.
+    expect(fs.existsSync(path.join(REPO_HOME, 'junit', 'junit', '4.13', 'junit-4.13.pom'))).toEqual(true);
+    expect(fs.existsSync(path.join(REPO_HOME, 'junit', 'junit', '4.12', 'junit-4.12.pom'))).toEqual(true);
+
+    // last updated files should always be deleted.
+    expect(fs.existsSync(path.join(REPO_HOME, 'junit', 'junit', '4.12', 'junit-4.12.jar.lastUpdated'))).toEqual(false);
+  });
+
+  test('It removes unaccessed cache entries', async () => {
+    // Last updated marker exists before test.
+    expect(fs.existsSync(path.join(REPO_HOME, 'junit', 'junit', '4.12', 'junit-4.12.jar.lastUpdated'))).toEqual(true);
+
+    core.getInput.mockReturnValueOnce('test-maven-12345');
+    core.getState.mockReturnValueOnce('test-maven-')
+      .mockReturnValueOnce(`${now}`);
+
+    cache.saveCache.mockResolvedValueOnce('12345');
+    await saveCache();
+
+    expect(core.getState).toHaveBeenNthCalledWith(1, 'maven.RestoredKey');
+    expect(core.getState).toHaveBeenNthCalledWith(2, 'maven.RestoredTime');
+
+    expect(fs.existsSync(path.join(REPO_HOME, 'junit', 'junit', '4.13', 'junit-4.13.pom'))).toEqual(true);
+    expect(fs.existsSync(path.join(REPO_HOME, 'junit', 'junit', '4.12', 'junit-4.12.pom'))).toEqual(false);
+
+    // last updated files should always be deleted.
+    expect(fs.existsSync(path.join(REPO_HOME, 'junit', 'junit', '4.12', 'junit-4.12.jar.lastUpdated'))).toEqual(false);
   });
 });
