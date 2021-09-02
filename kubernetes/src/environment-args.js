708414aa0046addbc89e3389eec0acb61792d2fa
Ostap <ostap.smolynets@extendaretail.com>
2021-09-02 18:12:38 +0300

    fix: Fix variable names

--- kubernetes/src/environment-args.js
+++ kubernetes/src/environment-args.js
@@ -5,8 +5,8 @@ const parseEnvironmentArgs = (environment, projectId) => {
       .replace('sm://*/', `sm://${projectId}/`);
     args[name] = value;
   });
-  args[SERVICE_PROJECT_ID] = projectId;
-  args[SERVICE_ENVIRONMENT] = projectId.includes('-staging-') ? 'staging' : 'prod';
+  args['SERVICE_PROJECT_ID'] = projectId;
+  args['SERVICE_ENVIRONMENT'] = projectId.includes('-staging-') ? 'staging' : 'prod';
   return args;
 };
 
