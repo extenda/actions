ba720caa0dd40c35694a8b7a13bb248d5368fe17
Vladyslav Bushynskyi <vladyslav.bushynskyi.ext@extendaretail.com>
2021-10-13 15:12:01 +0300

    feat: Add component tests action (#382)

---
+++ component-tests/src/index.js
@@ -0,0 +1,32 @@
+const { getInput, setFailed } = require('@actions/core');
+const { load: parseYaml } = require('js-yaml');
+const { readFile } = require('fs').promises;
+const { createApiTest } = require('./create-api-test');
+const { run } = require('../../utils');
+
+async function main() {
+  try {
+    const iamToken = getInput('auth-token', { required: true });
+    const url = getInput('base-url', { required: true });
+    const test = createApiTest(url, iamToken);
+
+    const testsFile = getInput('tests', { required: true });
+    const tests = parseYaml(await readFile(testsFile));
+
+
+    for (const [request, expected] of Object.entries(tests)) {
+      // eslint-disable-next-line no-await-in-loop
+      await test(request, expected);
+    }
+
+    await test.status();
+  } catch (error) {
+    setFailed(error.message);
+  }
+}
+
+if (require.main === module) {
+  run(main);
+}
+
+module.exports = main;
---
+++ component-tests/test/create-api-test.test.js
@@ -0,0 +1,42 @@
+const { load: parseYaml } = require('js-yaml');
+const { readFile } = require('fs').promises;
+const { resolve } = require('path');
+const { createApiTest } = require('../src/create-api-test');
+
+const spy = jest.fn();
+const mockCall = (method, url) => ({
+  set: () => ({
+    expect(res) {
+      spy(method, url, res);
+      return this;
+    },
+  }),
+});
+jest.mock('supertest', () => ({
+  agent: () => ({
+    post: (url) => mockCall('POST', url),
+    get: (url) => mockCall('GET', url),
+    auth: () => {},
+  }),
+}));
+
+describe('createApiTest function', () => {
+  test('it runs correct tests', async () => {
+    const yaml = parseYaml(await readFile(resolve(__dirname, '../src/tests.yml')));
+    const test = createApiTest('', '');
+    for (const [request, expected] of Object.entries(yaml)) {
+      // eslint-disable-next-line no-await-in-loop
+      await test(request, expected);
+    }
+    expect(spy).toBeCalledTimes(3);
+    expect(spy).toBeCalledWith('POST', '/api/v1/permissions', 400);
+    expect(spy).toBeCalledWith('GET', '/api/v1/permissions', 200);
+    expect(spy).toBeCalledWith('GET', '/api/v1/permissions', { field: 'value' });
+
+    try {
+      await test.status();
+    } catch (e) {
+      expect(e.message).toBe('Tests: 1 passed, 1 failed, 2 total');
+    }
+  });
+});
