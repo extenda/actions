6121706eece70b7e5c822ae76642d43c6081e506
Alexander Lundberg Santos <alexander.l.santos@extendaretail.com>
2021-10-21 10:38:37 +0200

    fix: Remove smells

--- publish-openapi/test/index.test.js
+++ publish-openapi/test/index.test.js
@@ -11,15 +11,15 @@ describe('Api documentation action', () => {
   });
 
   test('It can run the action', async () => {
-    core.getInput.mockReturnValueOnce('openapi.yaml')
-      .mockReturnValueOnce('api-name')
-      .mockReturnValueOnce('v1.0.0')
-      .mockReturnValueOnce('system-name')
-      .mockReturnValueOnce('gs://bucket');
+    // core.getInput.mockReturnValueOnce('openapi.yaml')
+    //   .mockReturnValueOnce('api-name')
+    //   .mockReturnValueOnce('v1.0.0')
+    //   .mockReturnValueOnce('system-name')
+    //   .mockReturnValueOnce('gs://bucket');
     // deployDocumentation.mockResolvedValueOnce({});
     await action();
 
-    expect(core.getInput).toHaveBeenCalledTimes(5);
+    expect(core.info).toHaveBeenCalledTimes(3);
     // expect(deployDocumentation).toHaveBeenCalledWith(
     //   'openapi.yaml',
     //   'api-name',
