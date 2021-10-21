4746bd011f38198b3c24e1271b703b7c514015ac
Alexander Lundberg Santos <alexander.l.santos@extendaretail.com>
2021-10-21 10:24:28 +0200

    feat: Disable publish open api action

--- publish-openapi/test/index.test.js
+++ publish-openapi/test/index.test.js
@@ -3,7 +3,7 @@ jest.mock('../src/redoc');
 
 const core = require('@actions/core');
 const action = require('../src/index');
-const deployDocumentation = require('../src/redoc');
+// const deployDocumentation = require('../src/redoc');
 
 describe('Api documentation action', () => {
   afterEach(() => {
@@ -16,16 +16,16 @@ describe('Api documentation action', () => {
       .mockReturnValueOnce('v1.0.0')
       .mockReturnValueOnce('system-name')
       .mockReturnValueOnce('gs://bucket');
-    deployDocumentation.mockResolvedValueOnce({});
+    // deployDocumentation.mockResolvedValueOnce({});
     await action();
 
     expect(core.getInput).toHaveBeenCalledTimes(5);
-    expect(deployDocumentation).toHaveBeenCalledWith(
-      'openapi.yaml',
-      'api-name',
-      'v1.0.0',
-      'gs://bucket',
-      'system-name',
-    );
+    // expect(deployDocumentation).toHaveBeenCalledWith(
+    //   'openapi.yaml',
+    //   'api-name',
+    //   'v1.0.0',
+    //   'gs://bucket',
+    //   'system-name',
+    // );
   });
 });
