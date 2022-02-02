ceb99c8249db9932aabc76e26afe33767f8e87ad
Samuel Sjoberg <samuel.sjoberg@extendaretail.com>
2022-02-02 12:14:42 +0100

    feat: Restrict deploy to master and main branch

--- cloud-run/src/index.js
+++ cloud-run/src/index.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
-const { run } = require('../../utils');
+const { run, failIfNotTrunkBased } = require('../../utils');
 const loadServiceDefinition = require('./service-definition');
 const runDeploy = require('./run-deploy');
 const configureDomains = require('./configure-domains');
@@ -13,25 +13,17 @@ const action = async () => {
   const dnsProjectLabel = core.getInput('dns-project-label') || 'dns';
   const verbose = (core.getInput('verbose') || 'false');
 
-  const isBranchMaster = () => {
-    const branch = process.env.GITHUB_REF.replace('refs/heads/', '');
-    // Return false if the branch is not master or main
-    return branch === 'master' || branch === 'main';
-  };
+  failIfNotTrunkBased();
 
-  if (isBranchMaster()) {
-    const service = loadServiceDefinition(serviceFile, jsonSchema);
-    await runDeploy(serviceAccountKey, service, image, verbose === 'true')
-      .then(({ cluster }) => configureDomains(
-        service,
-        cluster,
-        domainBindingsEnv,
-        dnsProjectLabel,
-        serviceAccountKey,
-      ));
-  } else {
-    throw new Error('Failed to deploy. You must follow trunk-based development and deploy from master or main branch only');
-  }
+  const service = loadServiceDefinition(serviceFile, jsonSchema);
+  await runDeploy(serviceAccountKey, service, image, verbose === 'true')
+    .then(({ cluster }) => configureDomains(
+      service,
+      cluster,
+      domainBindingsEnv,
+      dnsProjectLabel,
+      serviceAccountKey,
+    ));
 };
 
 if (require.main === module) {
---
+++ utils/src/trunk-killswitch.js
@@ -0,0 +1,15 @@
+const isAllowedBranch = (ref) => ref === 'refs/heads/master' || ref === 'refs/heads/main';
+
+/**
+ * Raise an error if the branch isn't a master or main branch. This helps us enforce trunk
+ * based workflows.
+ * @throws Error if invoked on an unexpected branch.
+ */
+const failIfNotTrunkBased = () => {
+  const ref = process.env.GITHUB_REF;
+  if (!isAllowedBranch(ref) && !ref.startsWith('refs/tags/')) {
+    throw new Error(`Action not allowed on ref ${ref}. You must follow trunk-based development and invoke this action from master, main or a release tag`);
+  }
+};
+
+module.exports = failIfNotTrunkBased;
