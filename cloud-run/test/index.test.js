f187e54c8d77598fbc616bf1340b8e61ce3c9e8b
Samuel Sjoberg <samuel.sjoberg@extendaretail.com>
2022-02-02 12:41:42 +0100

    chore: Add branch name tests

--- cloud-run/test/index.test.js
+++ cloud-run/test/index.test.js
@@ -8,19 +8,20 @@ const action = require('../src/index');
 const runDeploy = require('../src/run-deploy');
 const serviceDef = require('../src/service-definition');
 
-const env = {
-  ...process.env,
-};
+const orgEnv = process.env;
 
 describe('Cloud Run Action', () => {
   afterEach(() => {
     jest.resetAllMocks();
-    process.env = env;
-    delete process.env.GITHUB_REF;
+    process.env = orgEnv;
+  });
+
+  beforeEach(() => {
+    process.env = { ...orgEnv };
   });
 
   test('It can run the action', async () => {
-    process.env.GITHUB_REF = 'master';
+    process.env.GITHUB_REF = 'refs/heads/master';
     serviceDef.mockReturnValueOnce({});
     core.getInput.mockReturnValueOnce('service-account')
       .mockReturnValueOnce('cloud-run.yaml')
@@ -41,7 +42,7 @@ describe('Cloud Run Action', () => {
   });
 
   test('It can run without optional args', async () => {
-    process.env.GITHUB_REF = 'master';
+    process.env.GITHUB_REF = 'refs/heads/master';
     serviceDef.mockReturnValueOnce({});
     core.getInput.mockReturnValueOnce('service-account')
       .mockReturnValueOnce('')
@@ -63,7 +64,7 @@ describe('Cloud Run Action', () => {
   });
 
   test('It can run with verbose flag set', async () => {
-    process.env.GITHUB_REF = 'main';
+    process.env.GITHUB_REF = 'refs/heads/main';
     serviceDef.mockReturnValueOnce({});
     core.getInput.mockReturnValueOnce('service-account')
       .mockReturnValueOnce('')
@@ -85,7 +86,7 @@ describe('Cloud Run Action', () => {
   });
 
   test('It cannot run the action if branch is not master or main', async () => {
-    process.env.GITHUB_REF = 'develop';
+    process.env.GITHUB_REF = 'refs/heads/develop';
     serviceDef.mockReturnValueOnce({});
     core.getInput.mockReturnValueOnce('service-account')
       .mockReturnValueOnce('cloud-run.yaml')
@@ -93,7 +94,6 @@ describe('Cloud Run Action', () => {
       .mockReturnValueOnce('')
       .mockReturnValueOnce('dns')
       .mockReturnValueOnce('false');
-    await expect(action()).rejects
-      .toEqual(new Error('Failed to deploy. You must follow trunk-based development and deploy from master or main branch only'));
+    await expect(action()).rejects.toThrow(/^Action not allowed on ref refs\/heads\/develop/);
   });
 });
