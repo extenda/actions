bf59a5f7a7463bf955778eeff1eecd4d9e3a728e
Samuel Sjoberg <samuel.sjoberg@extendaretail.com>
2022-02-18 10:03:24 +0100

    fix: Pin gcloud to 365.0.0

--- setup-gcloud/src/index.js
+++ setup-gcloud/src/index.js
@@ -4,8 +4,7 @@ const setupGcloud = require('./setup-gcloud');
 
 run(async () => {
   const serviceAccountKey = core.getInput('service-account-key', { required: true });
-  // const version = core.getInput('version') || 'latest';
-  const version = '365.0.0';
+  const version = core.getInput('version') || 'latest';
   const exportCredentials = core.getInput('export-default-credentials') || 'false';
   await setupGcloud(serviceAccountKey, version, exportCredentials === 'true');
 });
