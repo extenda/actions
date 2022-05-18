c7f64709c4f6782e8fbf973e699fc34d76792481
Maxim DOVHOPOLYI <dovgopolyy0000@gmail.com>
2022-05-18 14:21:17 +0300

    feat(nexus-auth-npm): Add publish config flag (#473)

--- nexus-auth-npm/dist/index.js
+++ nexus-auth-npm/dist/index.js
@@ -52996,7 +52996,7 @@ exports.toCommandValue = toCommandValue;
 /***/ 8571:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
-const { getInput } = __webpack_require__(6341);
+const { getInput, getBooleanInput } = __webpack_require__(6341);
 const { run } = __webpack_require__(1898);
 const { validateOrFetchNexusCredentials } = __webpack_require__(6453);
 const { createNpmrcFile } = __webpack_require__(5859);
@@ -53006,6 +53006,7 @@ const action = async () => {
   const password = getInput('nexus-password') || process.env.NEXUS_PASSWORD;
   const serviceAccountKey = getInput('service-account-key');
   const outputDir = getInput('npmrc-dir') || '.';
+  const authForPublishing = getBooleanInput('auth-for-publishing') || false;
 
   const credentials = await validateOrFetchNexusCredentials({
     username,
@@ -53016,6 +53017,7 @@ const action = async () => {
   await createNpmrcFile({
     credentials,
     outputDir,
+    authForPublishing,
   });
 };
 
@@ -53082,15 +53084,22 @@ const { join } = __webpack_require__(5622);
  *     password: string,
  *   },
  *   outputDir: string,
+ *   authForPublishing?: boolean,
  * }}
  */
-const createNpmrcFile = async ({ credentials: { username, password }, outputDir }) => {
+const createNpmrcFile = async ({
+  credentials:
+  { username, password },
+  authForPublishing,
+  outputDir,
+}) => {
   const nexusToken = Buffer.from(`${username}:${password}`).toString('base64');
+  const repo = authForPublishing ? 'npm-private' : 'npm-group';
   const contents = `
-@hiiretail:registry = https://repo.extendaretail.com/repository/npm-group/
-//repo.extendaretail.com/repository/npm-group/:email = nexus@extenda.com
-//repo.extendaretail.com/repository/npm-group/:always-auth = true
-//repo.extendaretail.com/repository/npm-group/:_auth = ${nexusToken}
+@hiiretail:registry = https://repo.extendaretail.com/repository/${repo}/
+//repo.extendaretail.com/repository/${repo}/:email = nexus@extenda.com
+//repo.extendaretail.com/repository/${repo}/:always-auth = true
+//repo.extendaretail.com/repository/${repo}/:_auth = ${nexusToken}
 `;
   await writeFile(join(outputDir, '.npmrc'), contents);
 };
