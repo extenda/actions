826147c411c20aa60c58861c308726b7f1d8d40e
Alexander Lundberg Santos <AlexanderLundbergSantos@live.se>
2022-08-31 14:29:13 +0200

    chore: Update test pod to comply with new kubectl version (#524)

--- test-pod/src/run-pod.js
+++ test-pod/src/run-pod.js
@@ -95,7 +95,6 @@ const runPod = async ({ name, namespace }, image, configMap, trimPrefix) => {
     '--attach',
     '--restart=Never',
     '--pod-running-timeout=15m',
-    '--wait=true',
     `--image=${imageDigest}`,
     '-n',
     namespace,
@@ -114,7 +113,7 @@ const runPod = async ({ name, namespace }, image, configMap, trimPrefix) => {
   const json = JSON.stringify(
     createOverride(pod, namespace, imageDigest, configMap, serviceUrl, trimPrefix),
   );
-  args.push(`--overrides=${json}`);
+  args.push(`--overrides='${json}'`);
 
   let output = '';
   const filter = new LogFilter();
