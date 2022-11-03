f55cd00a543924d3c8d9795a6b476a314fd7026c
Samuel Sjöberg <samuel.sjoberg@extendaretail.com>
2022-11-03 12:42:39 +0100

    build: Inline conventionalcommits templates to fix build (#542)

---
+++ utils/scripts/inline-conventionalcommit-templates.js
@@ -0,0 +1,32 @@
+// Script that inlines handlebar templates in conventional commits. This helps NCC handle the
+// external files which otherwise are loaded numerous times and causing non-reproducible builds.
+const fs = require('fs');
+const path = require('path');
+
+const inlineTemplate = (source, moduleDir, template, replaceTextFn) => {
+  const value = fs.readFileSync(path.resolve(moduleDir, 'templates', template), 'utf-8');
+  return source.replace(replaceTextFn(template), JSON.stringify(value));
+};
+
+const inlineTemplates = (module, sourceFileName, replaceTextFn) => {
+  const moduleDir = path.resolve(__dirname, '..', 'node_modules', module);
+  const sourceFile = path.resolve(moduleDir, sourceFileName);
+  let source = fs.readFileSync(sourceFile, 'utf-8');
+  source = inlineTemplate(source, moduleDir, 'template.hbs', replaceTextFn);
+  source = inlineTemplate(source, moduleDir, 'header.hbs', replaceTextFn);
+  source = inlineTemplate(source, moduleDir, 'commit.hbs', replaceTextFn);
+  source = inlineTemplate(source, moduleDir, 'footer.hbs', replaceTextFn);
+  fs.writeFileSync(sourceFile, source, 'utf-8');
+};
+
+inlineTemplates(
+  'conventional-changelog-conventionalcommits',
+  'writer-opts.js',
+  (template) => `readFile(resolve(__dirname, './templates/${template}'), 'utf-8')`,
+);
+
+inlineTemplates(
+  'conventional-changelog-writer',
+  'index.js',
+  (template) => `readFileSync(join(__dirname, 'templates/${template}'), 'utf-8')`,
+);
