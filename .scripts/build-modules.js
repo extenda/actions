8a963c59b30be6514d7accc5fd5aecfde5cd102e
Maxim DOVHOPOLYI <dovgopolyy0000@gmail.com>
2022-11-16 22:42:59 +0200

    feat: Introduce esbuild

--- .scripts/build-modules.js
+++ .scripts/build-modules.js
@@ -1,18 +1,21 @@
-const fs = require('fs-extra');
-const path = require('path');
-const { modules } = require('./modules');
+const fs = require("fs-extra");
+const path = require("path");
+const { modules } = require("./modules");
 
-const ncc = '../node_modules/@vercel/ncc/dist/ncc/cli.js';
+const ncc = "../node_modules/.bin/esbuild";
 
-const build = (file, out = 'dist') => `node ${ncc} build ${file} -o ${out} --target es2018`;
+const build = (file, out = "dist/index.js") =>
+  `${ncc} ${file} --bundle --platform=node --outfile=${out} --target=es2018`;
 
 (async () => {
   modules.each((dir) => {
-    fs.removeSync(path.join(dir, 'dist'));
+    fs.removeSync(path.join(dir, "dist"));
   });
   await modules.exec((dir) => {
-    const commands = [build('src/index.js')];
-    const { actionHooks = {} } = JSON.parse(fs.readFileSync(path.join(dir, 'package.json')));
+    const commands = [build("src/index.js")];
+    const { actionHooks = {} } = JSON.parse(
+      fs.readFileSync(path.join(dir, "package.json"))
+    );
     Object.entries(actionHooks).forEach(([key, value]) => {
       commands.push(build(value, `dist/${key}`));
     });
