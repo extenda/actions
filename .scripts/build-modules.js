1d23e85b7f6e34b2c4bdb07498b753edfe7781a8
Maxim DOVHOPOLYI <dovgopolyy0000@gmail.com>
2022-11-18 01:03:22 +0200

    fix: Lint

--- .scripts/build-modules.js
+++ .scripts/build-modules.js
@@ -1,20 +1,19 @@
-const fs = require("fs-extra");
-const path = require("path");
-const { modules } = require("./modules");
+const fs = require('fs-extra');
+const path = require('path');
+const { modules } = require('./modules');
 
-const ncc = "../node_modules/.bin/esbuild";
+const ncc = '../node_modules/.bin/esbuild';
 
-const build = (file, out = "dist/index.js") =>
-  `${ncc} ${file} --bundle --platform=node --minify --outfile=${out} --target=es2018`;
+const build = (file, out = 'dist/index.js') => `${ncc} ${file} --bundle --platform=node --minify --outfile=${out} --target=es2018`;
 
 (async () => {
   modules.each((dir) => {
-    fs.removeSync(path.join(dir, "dist"));
+    fs.removeSync(path.join(dir, 'dist'));
   });
   await modules.exec((dir) => {
-    const commands = [build("src/index.js")];
+    const commands = [build('src/index.js')];
     const { actionHooks = {} } = JSON.parse(
-      fs.readFileSync(path.join(dir, "package.json"))
+      fs.readFileSync(path.join(dir, 'package.json')),
     );
     Object.entries(actionHooks).forEach(([key, value]) => {
       commands.push(build(value, `dist/${key}`));
