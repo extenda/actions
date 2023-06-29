8c32006dea199bd874c25917c648ac2e1dc247d5
Maxim DOVHOPOLYI <dovgopolyy0000@gmail.com>
2023-06-29 12:56:37 +0300

    fix: Add tests & lint

--- cloud-run-traffic/README.md
+++ cloud-run-traffic/README.md
@@ -6,9 +6,11 @@ A simple action for managing Cloud Run revisions traffic. Can be used for rollba
 
 See [action.yml](action.yml)
 
+> Note: this action support cloud run fully managed in clan projects. Cloud Run for Anthos support is not implemented yet.
+
 # Action setup example
 
-.github/workflows/exe.yml
+.github/workflows/manage-traffic-prod.yml
 ```yaml
 name: Manage Cloud Run Traffic [prod]
 on:
@@ -28,10 +30,10 @@ jobs:
     steps:
       - uses: actions/checkout@v1
 
-      - name: Customer config sync
+      - name: Manage Traffic
         uses: extenda/actions/cloud-run-traffic@v0
         with:
-          service-account-key: ${{ secrets.GCLOUD_AUTH }} # required
+          service-account-key: ${{ secrets.GCLOUD_AUTH_PROD }} # required
           # service-account-key: ${{ secrets.GCLOUD_AUTH_STAGING }} # staging
           service: ${{ github.event.inputs.service }} # required
           revision: ${{ github.event.inputs.revision }} # required
--- cloud-run-traffic/src/index.js
+++ cloud-run-traffic/src/index.js
@@ -1,17 +1,17 @@
-const core = require("@actions/core");
-const exec = require("@actions/exec");
-const { run } = require("../../utils/src");
-const setupGcloud = require("../../setup-gcloud/src/setup-gcloud");
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const { run } = require('../../utils/src');
+const setupGcloud = require('../../setup-gcloud/src/setup-gcloud');
 
 async function execGCloud(args) {
-  let stdout = "";
+  let stdout = '';
 
   core.info(`[running] gcloud "${args.join('" "')}"`);
-  await exec.exec("gcloud", args, {
+  await exec.exec('gcloud', args, {
     silent: false,
     listeners: {
       stdout: (data) => {
-        stdout += data.toString("utf8");
+        stdout += data.toString('utf8');
       },
     },
   });
@@ -22,8 +22,8 @@ async function execGCloud(args) {
 function validatePercentage(_percentage) {
   const percentage = +_percentage;
 
-  if (isNaN(percentage) || percentage < 0 || percentage > 100) {
-    throw new Error("Percentage must be a valid number between 0 and 100");
+  if (Number.isNaN(percentage) || percentage < 0 || percentage > 100) {
+    throw new Error('Percentage must be a valid number between 0 and 100');
   }
 
   return percentage;
@@ -31,48 +31,46 @@ function validatePercentage(_percentage) {
 
 function getRevisionsList(service, projectId) {
   return execGCloud([
-    "run",
-    "revisions",
-    "list",
+    'run',
+    'revisions',
+    'list',
     `--service=${service}`,
-    "--region=europe-west1",
+    '--region=europe-west1',
     `--project=${projectId}`,
     '--format="value[](name)"',
-  ]).then((str) => str.split("\n").map((s) => s.trim()));
+  ]).then((str) => str.split('\n').map((s) => s.trim()));
 }
 
 function routeTraffic(service, targetRevision, percentage, projectId) {
   return execGCloud([
-    "run",
-    "services",
-    "update-traffic",
+    'run',
+    'services',
+    'update-traffic',
     service,
     `--to-revisions=${targetRevision}=${percentage}`,
-    "--region=europe-west1",
+    '--region=europe-west1',
     `--project=${projectId}`,
   ]);
 }
 
 async function action() {
-  const serviceAccountKey = core.getInput("service-account-key", {
+  const serviceAccountKey = core.getInput('service-account-key', {
     required: true,
   });
-  const service = core.getInput("service", { required: true });
-  const targetRevision = core.getInput("target-revision", { required: true });
-  const percentage = validatePercentage(
-    core.getInput("percentage", { required: true }) || "100"
-  );
+  const service = core.getInput('service', { required: true });
+  const targetRevision = core.getInput('target-revision', { required: true });
+  const percentage = validatePercentage(core.getInput('percentage') || '100');
 
   const projectId = await setupGcloud(
     serviceAccountKey,
-    process.env.GCLOUD_INSTALLED_VERSION || "latest"
+    process.env.GCLOUD_INSTALLED_VERSION || 'latest',
   );
 
   const revisions = await getRevisionsList(service, projectId);
 
   if (!revisions.some((revision) => revision === targetRevision)) {
     throw new Error(
-      `Revision ${targetRevision} not found for service ${service}`
+      `Revision ${targetRevision} not found for service ${service}\n`,
     );
   }
 
