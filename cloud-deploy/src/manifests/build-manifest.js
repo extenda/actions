a7412100f99639bddae2cb53a8062036097bb9af
Alexander Lundberg Santos <AlexanderLundbergSantos@live.se>
2023-09-08 12:59:16 +0200

    fix: Statefulset volume claim (#622)

--- cloud-deploy/src/manifests/build-manifest.js
+++ cloud-deploy/src/manifests/build-manifest.js
@@ -142,6 +142,26 @@ const manifestTemplate = async (
                   memory: memoryRequest,
                 },
               },
+              ...(volumes && type.toLowerCase() === 'statefulset'
+              ? {
+                volumeClaimTemplates: [
+                  {
+                    metadata: {
+                      name: `${name}`,
+                    },
+                    spec: {
+                      accessModes: ['ReadWriteOnce'],
+                      storageClassName: volumes[0]['disk-type'] === 'hdd' ? 'standard' : 'premium-rwo',
+                      resources: {
+                        requests: {
+                          storage: volumes[0].size,
+                        },
+                      },
+                    },
+                  },
+                ],
+              }
+              : {}),
               env: environment.map((env) => ({
                 name: env.name,
                 value: env.value,
@@ -171,26 +191,6 @@ const manifestTemplate = async (
               : []),
           ],
           volumes: deploymentVolumes,
-          ...(volumes && type.toLowerCase() === 'statefulset'
-            ? {
-              volumeClaimTemplates: [
-                {
-                  metadata: {
-                    name: `${name}`,
-                  },
-                  spec: {
-                    accessModes: ['ReadWriteOnce'],
-                    storageClassName: volumes[0]['disk-type'] === 'hdd' ? 'standard' : 'premium-rwo',
-                    resources: {
-                      requests: {
-                        storage: volumes[0].size,
-                      },
-                    },
-                  },
-                },
-              ],
-            }
-            : {}),
         },
       },
     },
