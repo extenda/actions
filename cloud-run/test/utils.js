cc41471b7660bbb85999ac2c0b65aac791c15a97
Samuel Sjöberg <samuel.sjoberg@extendaretail.com>
2023-09-12 23:38:49 +0200

    fix: Restore cloud-run test utils

---
+++ cloud-run/test/utils.js
@@ -0,0 +1,10 @@
+const mockOutput = (data, opts) => {
+    if (opts && opts.listeners) {
+        opts.listeners.stdout(Buffer.from(`${data}\n`, 'utf8'));
+    }
+    return Promise.resolve(0);
+};
+
+module.exports = {
+    mockOutput,
+};
