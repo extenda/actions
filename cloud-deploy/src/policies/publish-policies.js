bc94b33f17870ee5913b6a39fee0bfab95e8a13f
Samuel Sjöberg <samuel.sjoberg@extendaretail.com>
2023-11-20 12:19:43 +0100

    fix(cloud-deploy): Mask nd_builtin_cache in OPA decision logs

--- cloud-deploy/src/policies/publish-policies.js
+++ cloud-deploy/src/policies/publish-policies.js
@@ -9,9 +9,9 @@ const DEFAULT_LOG_MASK = `package system.log
 
 # The policy below instructs OPA to remove all data from secrets before uploading the decision
 # https://www.openpolicyagent.org/docs/latest/decision-logs/#masking-sensitive-data
-# Note: this is an example; update the policy to reflect your configuration
 
 mask["/input/attributes/request/http/headers/authorization"]
+mask["/nd_builtin_cache"]
 `;
 
 const loadRego = (regoFile) => ({
