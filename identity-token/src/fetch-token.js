d69dff27624a6a40399187ee69a54572c041a713
Samuel Sjöberg <samuel.sjoberg@extendaretail.com>
2023-11-21 12:06:09 +0100

    fix(identity-token): Use execGcloud helper to show errors (#700)

--- identity-token/src/fetch-token.js
+++ identity-token/src/fetch-token.js
@@ -1,25 +1,12 @@
-const exec = require('@actions/exec');
+const { execGcloud } = require('../../setup-gcloud');
 
 // Fetch an identity token for the specified account
-const fetchToken = async (serviceAccount, audiences) => {
-  const args = [
-    'auth',
-    'print-identity-token',
-    `--impersonate-service-account=${serviceAccount}`,
-    '--include-email',
-    `--audiences=${audiences}`,
-  ];
-  let token = '';
-  await exec.exec('gcloud', args, {
-    silent: true,
-    listeners: {
-      stdout: (data) => {
-        token += data.toString('utf8');
-      },
-    },
-  });
-
-  return token.trim();
-};
+const fetchToken = async (serviceAccount, audiences) => execGcloud([
+  'auth',
+  'print-identity-token',
+  `--impersonate-service-account=${serviceAccount}`,
+  '--include-email',
+  `--audiences=${audiences}`,
+]);
 
 module.exports = fetchToken;
