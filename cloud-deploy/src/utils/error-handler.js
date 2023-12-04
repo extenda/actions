67f96882cc12c24eec8b21f6103220e19ffd2c94
Alexander Lundberg Santos <AlexanderLundbergSantos@live.se>
2023-12-04 17:12:20 +0100

    fix: Already in use should result in failure (#709)

--- cloud-deploy/src/utils/error-handler.js
+++ cloud-deploy/src/utils/error-handler.js
@@ -7,6 +7,11 @@ const handleError = async (errorMessage, action) => {
     message = 'Permissions are missing for your ci-cd account, verify you have all required permissions mentioned in the migration docs: https://docs.google.com/document/d/1uQjorCDupWN0i7MOK34f-RnkzgYvsnv24YdsXkAYEkE';
     throw new Error(message);
   }
+  if (errorMessage.includes('is already being used by')) {
+    // This failure should mean the loadbalancer backend can't be deleted
+    message = 'Unable to remove backend due to being used elsewhere, please contact platform team';
+    throw new Error(message);
+  }
   if (errorMessage.includes('already exists') || errorMessage.includes('Duplicate network endpoint')) {
     message = `${action} resulted in: already exists`;
   }
