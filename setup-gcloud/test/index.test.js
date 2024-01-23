9befa93b6e895ff05281750c7701da24a1976f3e
Samuel Sjöberg <samuel.sjoberg@extendaretail.com>
2024-01-23 23:52:31 +0100

    fix: Pin gcloud to 460.0.0 in action.yml until 461 is patched (#745)

---
+++ setup-gcloud/test/index.test.js
@@ -0,0 +1,22 @@
+const core = require('@actions/core');
+const { action } = require('../src/index');
+const setupGcloud = require('../src/setup-gcloud');
+
+jest.mock('../src/setup-gcloud');
+jest.mock('@actions/core');
+
+describe('setup-gcloud action', () => {
+  test('It can install latest', async () => {
+    core.getInput.mockReturnValueOnce('key')
+      .mockReturnValue('');
+    await action();
+    expect(setupGcloud).toHaveBeenCalledWith('key', 'latest', false);
+  });
+  test('It can install specified version', async () => {
+    core.getInput.mockReturnValueOnce('key')
+      .mockReturnValueOnce('300.0.0')
+      .mockReturnValue('');
+    await action();
+    expect(setupGcloud).toHaveBeenCalledWith('key', '300.0.0', false);
+  });
+});
