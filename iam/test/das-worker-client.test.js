227c6c40e7e4c06890108668de9c04aa27523308
Gleb Bashkatov <glebbash@gmail.com>
2024-01-30 11:42:27 +0000

    chore: fix

--- iam/test/das-worker-client.test.js
+++ iam/test/das-worker-client.test.js
@@ -3,22 +3,22 @@ const createDasWorkerClient = require('../src/das-worker-client');
 describe('Configure bundle sync', () => {
   it('points to `.com` for normal systems', () => {
     expect(
-      createDasWorkerClient('fsc.manual-sales-registration-ui-prod').defaults.baseUrl,
+      createDasWorkerClient('fsc.manual-sales-registration-ui-prod').defaults.baseURL,
     ).toBe('https://iam-das-worker.retailsvc.com/api/v1');
     expect(
-      createDasWorkerClient('lps.hii-high-risk-fetch-staging').defaults.baseUrl,
+      createDasWorkerClient('lps.hii-high-risk-fetch-staging').defaults.baseURL,
     ).toBe('https://iam-das-worker.retailsvc.com/api/v1');
     expect(
-      createDasWorkerClient('iam.iam-api-prod').defaults.baseUrl,
+      createDasWorkerClient('iam.iam-api-prod').defaults.baseURL,
     ).toBe('https://iam-das-worker.retailsvc.com/api/v1');
   });
 
   it('points to `.dev` for iam staging systems', () => {
     expect(
-      createDasWorkerClient('iam.iam-api-staging').defaults.baseUrl,
+      createDasWorkerClient('iam.iam-api-staging').defaults.baseURL,
     ).toBe('https://iam-das-worker.retailsvc.dev/api/v1');
     expect(
-      createDasWorkerClient('iam.iam-oauth-client-managment-staging').defaults.baseUrl,
+      createDasWorkerClient('iam.iam-oauth-client-managment-staging').defaults.baseURL,
     ).toBe('https://iam-das-worker.retailsvc.dev/api/v1');
   });
 });
