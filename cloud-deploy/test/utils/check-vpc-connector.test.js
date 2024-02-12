1e0db67ac97d47a61407cb5e44eacfbf158f86ab
Alexander Lundberg Santos <alexander.l.santos@extendaretail.com>
2024-02-12 11:44:37 +0100

    fix: Always check vpc connector

---
+++ cloud-deploy/test/utils/check-vpc-connector.test.js
@@ -0,0 +1,63 @@
+jest.mock('../../src/utils/gcloud-output');
+const checkVpcConnector = require('../../src/utils/check-vpc-connector');
+const gcloudOuput = require('../../src/utils/gcloud-output');
+
+const listVpcConnectorsWithoutMatchingName = [
+  {
+    machineType: 'e2-micro',
+    maxInstances: 3,
+    maxThroughput: 300,
+    minInstances: 2,
+    minThroughput: 200,
+    name: 'projects/some-project/locations/europe-west1/connectors/failing-connector',
+    network: 'some-network',
+    state: 'READY',
+    subnet: {
+      name: 'vpc-connector-subnet',
+      projectId: 'some-project',
+    },
+  },
+  {
+    machineType: 'e2-micro',
+    maxInstances: 3,
+    maxThroughput: 300,
+    minInstances: 2,
+    minThroughput: 200,
+    name: 'projects/some-project/locations/europe-west1/connectors/some-connector',
+    network: 'clan-network',
+    state: 'READY',
+    subnet: {
+      name: 'vpc-connector-subnet',
+      projectId: 'some-project',
+    },
+  },
+];
+
+describe('check vpc connector', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('It can return name if listed connectors match', async () => {
+    gcloudOuput.mockResolvedValueOnce(JSON.stringify(listVpcConnectorsWithoutMatchingName));
+
+    const connector = await checkVpcConnector('some-project', 'europe-west1', 'some-connector');
+    expect(connector).toEqual('some-connector');
+    expect(gcloudOuput).toHaveBeenCalledTimes(1);
+  });
+
+  test('It can return name if no match but connector exists on network', async () => {
+    gcloudOuput.mockResolvedValueOnce(JSON.stringify(listVpcConnectorsWithoutMatchingName));
+
+    const connector = await checkVpcConnector('some-project', 'europe-west1', 'some-connector123');
+    expect(connector).toEqual('some-connector');
+    expect(gcloudOuput).toHaveBeenCalledTimes(1);
+  });
+
+  test('It can fail if no connector on clan-network', async () => {
+    gcloudOuput.mockResolvedValueOnce(JSON.stringify([]));
+
+    await expect(checkVpcConnector('some-project', 'europe-west1', 'some-connector')).rejects.toThrow('No connector found for this project some-project');
+    expect(gcloudOuput).toHaveBeenCalledTimes(1);
+  });
+});
