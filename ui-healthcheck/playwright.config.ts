f9f80bf3cbd08f997f9eec8b08a22d16487d500f
artemka-debug <dovgopoly123@gmail.com>
2024-05-14 18:32:45 +0300

    feat(CHAP-548): add ui health checks

--- ui-healthcheck/playwright.config.ts
+++ ui-healthcheck/playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: './health-check',
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
