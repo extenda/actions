de243c03daae4c7df896308989a8e8c655999cf8
Alexander Lundberg Santos <AlexanderLundbergSantos@live.se>
2024-06-11 08:53:16 +0200

    feat: Disable publish-open-api action (#385)

--- publish-openapi/test/index.test.js
+++ publish-openapi/test/index.test.js
@@ -3,7 +3,7 @@ jest.mock('../src/redoc');
 
 const core = require('@actions/core');
 const action = require('../src/index');
-const deployDocumentation = require('../src/redoc');
+// const deployDocumentation = require('../src/redoc');
 
 describe('Api documentation action', () => {
   afterEach(() => {
@@ -11,21 +11,21 @@ describe('Api documentation action', () => {
   });
 
   test('It can run the action', async () => {
-    core.getInput.mockReturnValueOnce('openapi.yaml')
-      .mockReturnValueOnce('api-name')
-      .mockReturnValueOnce('v1.0.0')
-      .mockReturnValueOnce('system-name')
-      .mockReturnValueOnce('gs://bucket');
-    deployDocumentation.mockResolvedValueOnce({});
+    // core.getInput.mockReturnValueOnce('openapi.yaml')
+    //   .mockReturnValueOnce('api-name')
+    //   .mockReturnValueOnce('v1.0.0')
+    //   .mockReturnValueOnce('system-name')
+    //   .mockReturnValueOnce('gs://bucket');
+    // deployDocumentation.mockResolvedValueOnce({});
     await action();
 
-    expect(core.getInput).toHaveBeenCalledTimes(5);
-    expect(deployDocumentation).toHaveBeenCalledWith(
-      'openapi.yaml',
-      'api-name',
-      'v1.0.0',
-      'gs://bucket',
-      'system-name',
-    );
+    expect(core.info).toHaveBeenCalledTimes(3);
+    // expect(deployDocumentation).toHaveBeenCalledWith(
+    //   'openapi.yaml',
+    //   'api-name',
+    //   'v1.0.0',
+    //   'gs://bucket',
+    //   'system-name',
+    // );
   });
 });
