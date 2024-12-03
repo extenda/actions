940f7638919d8aaeed53e835f3d7a5c423be9150
Alexander Lundberg Santos <AlexanderLundbergSantos@live.se>
2024-12-03 14:22:23 +0100

    feat: Remove all loadbalancing handling from action (#996)

--- cloud-deploy/src/index.js
+++ cloud-deploy/src/index.js
@@ -9,16 +9,7 @@ const readSecret = require('./utils/load-credentials');
 const runScan = require('./utils/vulnerability-scanning');
 const getImageWithSha256 = require('./manifests/image-sha256');
 const publishPolicies = require('./policies/publish-policies');
-const createExternalLoadbalancer = require('./loadbalancing/external/create-external-loadbalancer');
-const configureInternalDomain = require('./loadbalancing/internal/create-internal-backend');
-const configureExternalDomain = require('./loadbalancing/external/create-external-backend');
-const configureExternalLBFrontend = require('./loadbalancing/external/create-external-frontend');
-const configureInternalFrontend = require('./loadbalancing/internal/create-internal-frontend');
-const {
-  checkPolicyExists,
-  setCloudArmorPolicyTarget,
-  checkPolicyTarget,
-} = require('./loadbalancing/external/cloud-armor');
+const checkPolicyExists = require('./utils/cloud-armor');
 const {
   sendScaleSetup,
   sendDeployInfo,
@@ -207,54 +198,6 @@ const action = async () => {
           slackChannel,
         ),
       );
-
-      await createExternalLoadbalancer(projectID, env);
-      if (domainMappings) {
-        await configureExternalLBFrontend(
-          projectID,
-          env,
-          [...domainMappings],
-          migrate,
-        );
-        await configureExternalDomain(
-          projectID,
-          serviceName,
-          env,
-          domainMappings,
-          protocol,
-          timeout,
-          platformGKE,
-        );
-        const updateCloudArmorPolicy = await checkPolicyTarget(
-          serviceName,
-          cloudArmorPolicy,
-          projectID,
-        );
-        if (!updateCloudArmorPolicy) {
-          await setCloudArmorPolicyTarget(
-            serviceName,
-            cloudArmorPolicy,
-            projectID,
-          );
-        }
-      }
-      if (internalTraffic) {
-        await configureInternalDomain(
-          projectID,
-          serviceName,
-          env,
-          protocol,
-          timeout,
-          platformGKE,
-        );
-        await configureInternalFrontend(
-          projectID,
-          serviceName,
-          env,
-          protocol,
-          platformGKE,
-        );
-      }
 
       await Promise.all(requests);
     }
