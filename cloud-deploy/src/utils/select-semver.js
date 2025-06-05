3859d8966943533e3168f51c2ba01f72b9017156
Samuel Sjöberg <samuel.sjoberg@extendaretail.com>
2025-06-05 10:05:29 +0200

    fix: Add missing file

---
+++ cloud-deploy/src/utils/select-semver.js
@@ -0,0 +1,22 @@
+const { SemVer, valid } = require('semver');
+
+/**
+ * Ensure that a preview semantic version only gets used if it is semantically greater than the stable version.
+ * @param previewVersion a preview semantic version
+ * @param stableVersion a stable semantic version
+ * @return the version to use
+ */
+const selectSemver = (previewVersion, stableVersion) => {
+  if (!valid(previewVersion)) {
+    // Preview is not a semver. Typically this means it is a tag such as 'beta' or similar.
+    return previewVersion;
+  }
+  const value = new SemVer(previewVersion).compare(new SemVer(stableVersion));
+  if (value > 0) {
+    // Preview is greater than the stable version.
+    return previewVersion;
+  }
+  return stableVersion;
+};
+
+module.exports = selectSemver;
