f17235a676d5c9e26877735814a256aa59a55401
Samuel Sjöberg <samuel.sjoberg@extendaretail.com>
2025-10-24 18:44:02 +0200

    fix(maven): Restore previous gcloud account after mvn (#1160)

---
+++ setup-gcloud/src/with-gcloud.js
@@ -0,0 +1,54 @@
+const core = require('@actions/core');
+const setupGcloud = require('./setup-gcloud');
+const { execGcloud } = require('./exec-gcloud');
+
+const getGcloudAccount = async () =>
+  execGcloud(['config', 'get', 'account', '--format=json'], 'gcloud', true)
+    .then(JSON.parse)
+    .then((account) => {
+      if (typeof account === 'string' && account.length > 0) {
+        return account;
+      }
+      return null;
+    })
+    .catch(() => null);
+
+const restorePreviousGcloudAccount = async (previousAccount) => {
+  const current = await getGcloudAccount();
+  if (previousAccount !== current) {
+    core.info(`Restore gcloud account ${previousAccount}`);
+    await execGcloud(
+      ['config', 'set', 'account', previousAccount],
+      'gcloud',
+      true,
+    );
+  }
+};
+
+/**
+ * Execute <code>fn</code> with gcloud signed in with the provided service account.
+ * Gcloud is restored to its previous state before the method returns.
+ * @template T the type returned by the callback function <code>fn</code>
+ * @param {string} serviceAccountKey the google cloud service account key
+ * @param {function(string): Promise<T>} fn an async function to execute
+ * @return {Promise<T>} a promise that completes when the function has executed
+ */
+const withGcloud = async (serviceAccountKey, fn) => {
+  let previousAccount;
+  if (process.env.GCLOUD_INSTALLED_VERSION) {
+    previousAccount = await getGcloudAccount();
+  }
+
+  let result;
+  try {
+    const projectId = await setupGcloud(serviceAccountKey);
+    result = await fn(projectId);
+  } finally {
+    if (previousAccount) {
+      await restorePreviousGcloudAccount(previousAccount);
+    }
+  }
+  return result;
+};
+
+module.exports = withGcloud;
