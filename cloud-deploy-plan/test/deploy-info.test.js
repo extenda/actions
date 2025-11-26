c79ef3c82cb442ed3750503a534f307b402ceaa4
Samuel Sjöberg <samuel.sjoberg@extendaretail.com>
2025-11-26 22:18:41 +0100

    chore: Improve test

--- cloud-deploy-plan/test/deploy-info.test.js
+++ cloud-deploy-plan/test/deploy-info.test.js
@@ -4,18 +4,18 @@ const { execGcloud } = require('../../setup-gcloud');
 
 jest.mock('../../setup-gcloud');
 
+let token;
+
+beforeEach(async () => {
+  execGcloud.mockResolvedValueOnce('token');
+  token = await execGcloud(['auth', 'print-identity-token'], 'gcloud', true);
+});
+
 afterEach(() => {
   nock.cleanAll();
 });
 
 test('Get deploy info', async () => {
-  execGcloud.mockResolvedValueOnce('token');
-  const token = await execGcloud(
-    ['auth', 'print-identity-token'],
-    'gcloud',
-    true,
-  );
-
   const scope = nock('https://platform-api.retailsvc.com')
     .post('/info/deploy')
     .once()
@@ -55,5 +55,51 @@ test('Get deploy info', async () => {
     updates: false,
     vulnerabilities: false,
     serveTraffic: true,
+  });
+});
+
+test('Get deploy info with canary deployment', async () => {
+  const scope = nock('https://platform-api.retailsvc.com')
+    .post('/info/deploy')
+    .once()
+    .reply(200, {
+      serviceName: 'checkout-service-manager-api',
+      updates: false,
+      vulnerabilities: false,
+    });
+
+  const deployInfo = await getDeployInfo(
+    {
+      'cloud-run': {
+        service: 'testrunner-eu-checkout-api',
+        protocol: 'http',
+        timeout: 300,
+        traffic: {
+          'serve-traffic': false,
+        },
+      },
+      security: 'none',
+      environments: {
+        production: {
+          'domain-mappings': ['checkout-service-manager.retailsvc.com'],
+          'path-mappings': [
+            {
+              paths: ['/platform-api/health', '/platform-api/health/*'],
+              service: 'hiiretail-platform-api',
+              'path-rewrite': '/health',
+            },
+          ],
+        },
+      },
+    },
+    'platform-prod-2481',
+    token,
+  );
+  expect(scope.isDone()).toEqual(true);
+  expect(deployInfo).toEqual({
+    serviceName: 'checkout-service-manager-api',
+    updates: false,
+    vulnerabilities: false,
+    serveTraffic: false,
   });
 });
