f117a722e5670a3e969e4cfc3f4ccc103cc1199f
Alexander <alexander.l.santos@extendaretail.com>
2026-03-05 07:37:27 +0100

    fix: Run lint

--- cloud-run/src/index.js
+++ cloud-run/src/index.js
@@ -1,7 +1,6 @@
 import * as core from '@actions/core';
 
 const action = async () => {
-
   core.warning('This action is deprecated and has been disabled!');
   core.warning(
     'Please migrate to the new cloud-deploy Action for continued support and new features.',
