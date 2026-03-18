cf6f6121fa74ea283a1a9df9a606ddddf4dabef2
Samuel Sjöberg <samuel.sjoberg@extendaretail.com>
2026-03-18 12:47:12 +0100

    chore: Fix failing test

--- binary-auth-attestation/test/create-sign-attestion.test.js
+++ binary-auth-attestation/test/create-sign-attestion.test.js
@@ -1,13 +1,16 @@
-import { afterEach, describe, expect, test, vi } from 'vitest';
-vi.mock('../../setup-gcloud/src/index.js');
-vi.mock('@actions/exec');
 import { getExecOutput } from '@actions/exec';
+import { afterEach, describe, expect, test, vi } from 'vitest';
 
+import getImageWithSha256 from '../../cloud-deploy/src/manifests/image-sha256.js';
 import { execGcloud } from '../../setup-gcloud/src/index.js';
 import {
   createAttestation,
   getArtifactUrl,
 } from '../src/create-sign-attestion.js';
+
+vi.mock('../../setup-gcloud/src/index.js');
+vi.mock('@actions/exec');
+vi.mock('../../cloud-deploy/src/manifests/image-sha256.js');
 
 describe('Create attestation', () => {
   afterEach(() => {
@@ -26,7 +29,10 @@ describe('Create attestation', () => {
       '1',
     );
     getExecOutput.mockResolvedValueOnce(JSON.stringify({}));
-    execGcloud.mockResolvedValueOnce('digest2626');
+    // execGcloud.mockResolvedValueOnce('digest2626');
+
+    getImageWithSha256.mockResolvedValueOnce('eu.gcr.io/my-iamge@digest2626');
+
     expect(execGcloud).toHaveBeenCalledTimes(1);
     expect(execGcloud).toHaveBeenCalledWith([
       '--quiet',
@@ -47,75 +53,18 @@ describe('Create attestation', () => {
   });
 
   test('Get artifact URL with default tag', async () => {
-    const imagePath = 'eu.gcr.io/my-image';
-    const digest = 'djdq1787';
-    getExecOutput.mockResolvedValueOnce({ stdout: JSON.stringify({}) });
-    execGcloud.mockResolvedValueOnce('').mockResolvedValueOnce(digest);
+    getImageWithSha256.mockResolvedValueOnce('eu.gcr.io/my-image@djdq1787');
 
-    expect(await getArtifactUrl('tag', imagePath)).toEqual(
+    expect(await getArtifactUrl('tag', 'eu.gcr.io/my-image')).toEqual(
       'eu.gcr.io/my-image@djdq1787',
     );
-    expect(getExecOutput).toHaveBeenCalledWith('docker', [
-      'manifest',
-      'inspect',
-      'eu.gcr.io/my-image:tag',
-    ]);
-    expect(execGcloud).toHaveBeenCalledTimes(2);
+    expect(getImageWithSha256).toHaveBeenCalledWith('eu.gcr.io/my-image:tag');
   });
 
   test('Get artifact URL provided a tag in the imagePath', async () => {
-    const imagePath = 'eu.gcr.io/my-image:tag1';
-    const digest = 'dut6h1787';
-    getExecOutput.mockResolvedValueOnce({ stdout: JSON.stringify({}) });
-    execGcloud.mockResolvedValueOnce('').mockResolvedValueOnce(digest);
-
-    expect(await getArtifactUrl('tag1', imagePath)).toEqual(
+    getImageWithSha256.mockResolvedValueOnce('eu.gcr.io/my-image@dut6h1787');
+    expect(await getArtifactUrl('tag1', 'eu.gcr.io/my-image:tag1')).toEqual(
       'eu.gcr.io/my-image@dut6h1787',
-    );
-    expect(getExecOutput).toHaveBeenCalledTimes(1);
-    expect(execGcloud).toHaveBeenCalledTimes(2);
-  });
-
-  test('Get artifact URL with provenance true', async () => {
-    const imagePath = 'eu.gcr.io/my-image:tag1';
-    getExecOutput.mockResolvedValueOnce({
-      stdout: JSON.stringify({
-        manifests: [
-          {
-            platform: { architecture: 'amd64', os: 'linux' },
-            digest: 'dut6h1787',
-          },
-          {
-            platform: { architecture: 'unknown', os: 'unknown' },
-            digest: 'wrong',
-          },
-        ],
-      }),
-    });
-    execGcloud.mockResolvedValueOnce('');
-
-    expect(await getArtifactUrl('tag1', imagePath)).toEqual(
-      'eu.gcr.io/my-image@dut6h1787',
-    );
-    expect(getExecOutput).toHaveBeenCalledTimes(1);
-    expect(execGcloud).toHaveBeenCalledTimes(1);
-  });
-
-  test('Get artifact URL with no digest', async () => {
-    const imagePath = 'eu.gcr.io/my-image:tag1';
-    getExecOutput.mockResolvedValueOnce({
-      stdout: JSON.stringify({
-        manifests: [
-          {
-            platform: { architecture: 'amd64', os: 'linux' },
-          },
-        ],
-      }),
-    });
-    execGcloud.mockResolvedValueOnce('').mockResolvedValueOnce('');
-
-    await expect(getArtifactUrl('tag1', imagePath)).rejects.toThrow(
-      'Failed to retrieve digest for image eu.gcr.io/my-image:tag1',
     );
   });
 });
