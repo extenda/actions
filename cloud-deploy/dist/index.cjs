c98d7459fcdd69ab6c71ec93d7bb5e32fc91bab8
Alexander <alexander.l.santos@extendaretail.com>
2026-03-20 10:55:21 +0100

    chore: Pin trivy version

--- cloud-deploy/dist/index.cjs
+++ cloud-deploy/dist/index.cjs
@@ -104728,7 +104728,7 @@ __name(uploadSbom, "uploadSbom");
 var DEFAULT_ATTESTATION_KEY_URI = "gcpkms://projects/platform-prod-2481/locations/europe-west1/keyRings/sbom-keyring/cry\
 ptoKeys/sbom-attestor-key/cryptoKeyVersions/1";
 var trivy = /* @__PURE__ */ __name(async (serviceAccountKey, image, {
-  version: version3,
+  version: version3 = "v0.69.3",
   severity,
   ignoreUnfixed,
   timeout,
