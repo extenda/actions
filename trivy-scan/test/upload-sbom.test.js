99b5fc23b04940feff798c91e4fed4252a4eca9f
Samuel Sjöberg <samuel.sjoberg@extendaretail.com>
2026-03-25 09:44:22 +0100

    chore: Remove create-key-file

--- trivy-scan/test/upload-sbom.test.js
+++ trivy-scan/test/upload-sbom.test.js
@@ -3,7 +3,6 @@ import { exec } from '@actions/exec';
 import { afterEach, beforeEach, expect, test, vi } from 'vitest';
 
 import { execGcloud } from '../../setup-gcloud/src/index.js';
-import createKeyFile from '../../utils/src/create-key-file.js';
 import { resolveImageDigests } from '../../utils/src/index.js';
 import setupCosign from '../src/setup-cosign.js';
 import uploadSbom from '../src/upload-sbom.js';
@@ -14,7 +13,6 @@ vi.mock('../../setup-gcloud/src/index.js');
 vi.mock('../src/resolve-digest.js');
 vi.mock('../src/setup-cosign.js');
 vi.mock('../../utils/src/index.js');
-vi.mock('../../utils/src/create-key-file.js');
 
 afterEach(() => {
   vi.resetAllMocks();
@@ -22,7 +20,6 @@ afterEach(() => {
 
 beforeEach(() => {
   execGcloud.mockResolvedValue(undefined);
-  createKeyFile.mockReturnValue('google-key.json');
   exec.mockResolvedValue(0);
   resolveImageDigests.mockResolvedValue({
     indexSha: 'eu.gcr.io/extenda/test@sha256:index',
